fe0e9ab7f1fe3c53102d607bc32728be267e10a8
Dave <pucklord322@gmail.com>
2026-09-24 16:32:58 +0800

    Better Handling of Security

--- CRMSystem/Program.cs
+++ CRMSystem/Program.cs
@@ -15,6 +15,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
+DotNetEnv.Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpContextAccessor();
